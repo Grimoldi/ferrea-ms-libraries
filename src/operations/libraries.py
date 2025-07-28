30c4dcc7fb53ddc1051900aa68d0971b82f5843d
Grimoldi Eugenio <11293040+Grimoldi@users.noreply.github.com>
2025-07-28 23:30:01 +0200

    Added update and delete operation.

--- src/operations/libraries.py
+++ src/operations/libraries.py
@@ -1,3 +1,5 @@
+from ferrea.observability.logs import ferrea_logger
+
 from models.exceptions import FerreaNonExistingLibrary
 from models.library import Library
 from models.repository import RepositoryService
@@ -12,11 +14,10 @@ def get_all_libraries(repository: RepositoryService) -> list[Library]:
     Returns:
         list[Library]: the list of all libraries in the repository.
     """
-
     return repository.find_all_libraries()
 
 
-def get_a_library_by_fid(repository: RepositoryService, fid: str) -> Library | None:
+def get_library_by_fid(repository: RepositoryService, fid: str) -> Library | None:
     """Search for a specific library in the repository.
 
     Args:
@@ -24,7 +25,7 @@ def get_a_library_by_fid(repository: RepositoryService, fid: str) -> Library | N
         name (str): the name of the library.
 
         Returns:
-            Library: the library found or None.
+            Library | None: the library found or None if not found.
     """
     try:
         return repository.find_a_library_by_fid(fid)
@@ -32,7 +33,7 @@ def get_a_library_by_fid(repository: RepositoryService, fid: str) -> Library | N
         return None
 
 
-def upsert_a_library(repository: RepositoryService, library: Library) -> Library:
+def upsert_library(repository: RepositoryService, library: Library) -> Library:
     """Created a new library or update an already existing one.
 
     Args:
@@ -43,3 +44,45 @@ def upsert_a_library(repository: RepositoryService, library: Library) -> Library
         Library: the created Library.
     """
     return repository.create_library(library)
+
+
+def update_library(
+    repository: RepositoryService,
+    fid: str,
+    new_library: Library,
+) -> Library | None:
+    """Update an already existing library.
+
+    Args:
+        repository (RepositoryService): the repository instance.
+        fid (str): the object fid (ferrea id)
+        library (Library): the library data.
+
+    Returns:
+        Library | None: the updatedlibrary or None if not found.
+    """
+    ferrea_logger.info(f"Start updating library with fid {fid}.")
+    try:
+        return repository.update_library(fid, new_library)
+    except FerreaNonExistingLibrary as _:
+        return None
+
+
+def delete_library(
+    repository: RepositoryService,
+    fid: str,
+) -> Library | None:
+    """Deletes an already existing library.
+
+    Args:
+        repository (RepositoryService): the repository instance.
+        fid (str): the object fid (ferrea id)
+        library (Library): the library data.
+
+    Returns:
+        Library | None: the deleted or None if not found.
+    """
+    try:
+        return repository.delete_library(fid)
+    except FerreaNonExistingLibrary as _:
+        return None
